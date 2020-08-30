5206fbcedfdf006a060171aa863bc99fda0990b4
Gianluca Bargelli <gianluca@callbell.eu>
2020-08-30 16:43:24 +0000

    enh: add aria-label attribute to icon links, specify lang attribute on body

--- pages/index.js
+++ pages/index.js
@@ -46,9 +46,9 @@ export default function Home() {
           <p className="mt-6">I'm currently co-founder and CTO of <a href="https://callbell.eu" target="_blank" rel="noopener" className="hover:underline text-blue-400 dark:text-orange-400">Callbell</a>, a shared collaborative inbox for Facebook Messenger and WhatsApp.</p>
         </div>
         <div className="mt-10 flex flew-row align-center items-center">
-          <div className="mr-3"><a href="https://github.com/proudlygeek" target="_blank" rel="noopener" className="hover:text-blue-400 dark:hover:text-orange-400 transition-colors duration-300"><GithubIcon /></a></div>
-          <div className="mr-3"><a href="https://www.linkedin.com/in/gianlucabargelli/" target="_blank" rel="noopener" className="hover:text-blue-400 dark:hover:text-orange-400 transition-colors duration-300"><LinkedInIcon /></a></div>
-          <div><a href="mailto:g.bargelli@gmail.com" target="_blank" rel="noopener" className="hover:text-blue-400 dark:hover:text-orange-400 transition-colors duration-300"><EmailIcon /></a></div>
+          <div className="mr-3"><a href="https://github.com/proudlygeek" target="_blank" rel="noopener" aria-label="Visit GitHub" className="hover:text-blue-400 dark:hover:text-orange-400 transition-colors duration-300"><GithubIcon /></a></div>
+          <div className="mr-3"><a href="https://www.linkedin.com/in/gianlucabargelli/" target="_blank" rel="noopener" aria-label="Visit LinkedIn" className="hover:text-blue-400 dark:hover:text-orange-400 transition-colors duration-300"><LinkedInIcon /></a></div>
+          <div><a href="mailto:g.bargelli@gmail.com" target="_blank" rel="noopener" aria-label="Send Email" className="hover:text-blue-400 dark:hover:text-orange-400 transition-colors duration-300"><EmailIcon /></a></div>
         </div>
       </main>
     </div>
